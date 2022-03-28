35a0cccd188748f45d79695d065334344b92729e
swardana <swardana@tutanota.com>
2022-03-28 14:21:19 +0700

    Add MenuView API contract

---
+++ src/main/java/com/swardana/nayanika/gui/topbar/menu/MenuView.java
@@ -0,0 +1,31 @@
+/*
+ * Nayanika is a picture viewer application.
+ * Copyright (C) 2022  Sukma Wardana
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.swardana.nayanika.gui.topbar.menu;
+
+import com.swardana.nayanika.gui.View;
+
+/**
+ * A view contract for menu visual.
+ *
+ * @author Sukma Wardana
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public interface MenuView extends View {
+}
