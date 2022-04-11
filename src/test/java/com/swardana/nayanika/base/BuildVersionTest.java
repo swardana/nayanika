d8bc3bbdc50d89d601b205c62edd12ff74dadf51
swardana <swardana@tutanota.com>
2022-04-11 10:06:37 +0700

    Mark for next milestone development

--- src/test/java/com/swardana/nayanika/base/BuildVersionTest.java
+++ src/test/java/com/swardana/nayanika/base/BuildVersionTest.java
@@ -43,7 +43,7 @@ class BuildVersionTest {
     @Test
     @DisplayName("Test fetch the current build version ")
     public void testCurrentBuildVersion() {
-        var expected = "1.0-M1";
+        var expected = "1.0-SNAPSHOT";
         var actual = new BuildVersion().buildVersion();
         assertThat(actual).isNotNull().isEqualTo(expected);
     }
