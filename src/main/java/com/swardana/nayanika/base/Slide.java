6587942d59a6bd4380345d990954ab58cd8bb109
swardana <swardana@tutanota.com>
2022-04-22 09:29:18 +0700

    Add slide-show control API

--- src/main/java/com/swardana/nayanika/base/Slide.java
+++ src/main/java/com/swardana/nayanika/base/Slide.java
@@ -18,11 +18,15 @@
 
 package com.swardana.nayanika.base;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.util.Duration;
 
 /**
  * A slide-show.
@@ -78,5 +82,101 @@ public interface Slide {
      * @return {@code true} if the slide-show operation is running.
      */
     ReadOnlyBooleanProperty isRunningProperty();
+
+    /**
+     * A slide-show control.
+     * <p>
+     *     A slide-show operation using JavaFX {@link Timeline} API.
+     * </p>
+     *
+     * @author Sukma Wardana
+     * @version 1.0.0
+     * @since 1.0.0
+     */
+    class Control implements Slide {
+
+        private static final Logger LOG = Logger.getLogger(Control.class.getName());
+
+        private static final double DEFAULT_DURATION = 9.0;
+
+        private final BooleanProperty running;
+        private final Timeline timeline;
+        private double time;
+        private PicturesNavigation pagination;
+
+        /**
+         * Creates new Slide::Control.
+         *
+         * @param duration the time duration of slide transition.
+         * @param nav the picture gallery navigation.
+         */
+        public Control(final double duration, final PicturesNavigation nav) {
+            this();
+            this.time = duration;
+            this.pagination = nav;
+        }
+
+        /**
+         * Creates new Slide::Control.
+         */
+        public Control() {
+            this.timeline = new Timeline();
+            this.running = new SimpleBooleanProperty(this, "running", false);
+            this.time = DEFAULT_DURATION;
+        }
+
+        @Override
+        public final void play() {
+            if (this.pagination == null) {
+                LOG.log(
+                    Level.WARNING,
+                    "The PicturesNavigation is NULL, will not play the slide-show"
+                );
+                return;
+            }
+            var keyFrame = new KeyFrame(
+                Duration.seconds(this.time),
+                e -> {
+                    this.pagination.next();
+                }
+            );
+            this.timeline.getKeyFrames().addAll(keyFrame);
+            this.timeline.setCycleCount(Timeline.INDEFINITE);
+            this.timeline.play();
+        }
+
+        @Override
+        public final void stop() {
+            if (
+                this.timeline.getStatus().equals(Timeline.Status.STOPPED)
+                || this.timeline.getStatus().equals(Timeline.Status.PAUSED)
+            ) {
+                LOG.log(Level.WARNING, "The Slide is not running, do nothing!");
+                return;
+            }
+            this.timeline.stop();
+            this.timeline.getKeyFrames().clear();
+        }
+
+        @Override
+        public final void updateTime(final double duration) {
+            LOG.log(
+                Level.FINE, "Update the time duration from {0}, to {1}",
+                new Object[]{this.time, duration}
+            );
+            this.time = duration;
+        }
+
+        @Override
+        public final void updateControl(final PicturesNavigation nav) {
+            this.pagination = nav;
+        }
+
+        @Override
+        public final ReadOnlyBooleanProperty isRunningProperty() {
+            return this.running;
+        }
+
+    }
 
 }
